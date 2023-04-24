04f1d365f1d2aab6871c1031615ffe00c1cf3b62
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2023-04-24 22:09:16 +0530

    Data Encapsulation

---
+++ app.js
@@ -0,0 +1,15 @@
+// Data Encapsulation
+function createPerson (name) {
+  return {
+    name: name,
+    age: 29,
+    greetings: function () {
+      console.log(`My Name is ${this.name}`)
+    }
+  }
+}
+
+// let person1 = createPerson('Muthu')
+// person1.greetings()
+
+createPerson('Muthu').greetings()
